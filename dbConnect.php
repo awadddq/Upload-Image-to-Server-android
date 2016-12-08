481c97cd60389fc2c34632a4765f838d70797d2c
almkhrfich <almkhrfich@gmail.com>
2016-12-08 23:14:57 +0900

    android upload app by php

---
+++ dbConnect.php
@@ -0,0 +1,8 @@
+<?php 
+
+	define('HOST','mysql.hostinger.in');
+	define('USER','u502452270_andro');
+	define('PASS','belal_123');
+	define('DB','u502452270_andro');
+	
+	$con = mysqli_connect(HOST,USER,PASS,DB) or die('unable to connect to db');
